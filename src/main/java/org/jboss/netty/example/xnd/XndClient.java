02e430a4dee731c57b6e91efaf68fc70b8b9bbd4
CliffYuan <xiaoniudu2012@gmail.com>
2014-04-30 18:16:51 +0800

    add example log

---
+++ src/main/java/org/jboss/netty/example/xnd/XndClient.java
@@ -0,0 +1,11 @@
+package org.jboss.netty.example.xnd;
+
+/**
+ * Created by yuanyuanming on 14-4-30.
+ */
+public class XndClient {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/org/jboss/netty/example/xnd/XndServer.java
@@ -0,0 +1,36 @@
+package org.jboss.netty.example.xnd;
+
+import org.jboss.netty.bootstrap.ServerBootstrap;
+import org.jboss.netty.channel.ChannelPipeline;
+import org.jboss.netty.channel.ChannelPipelineFactory;
+import org.jboss.netty.channel.Channels;
+import org.jboss.netty.channel.DefaultChannelPipeline;
+import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
+import org.jboss.netty.handler.codec.string.StringDecoder;
+import org.jboss.netty.handler.codec.string.StringEncoder;
+
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+
+/**
+ * Created by yuanyuanming on 14-4-30.
+ */
+public class XndServer {
+
+    public static void main(String[] args) {
+
+        ServerBootstrap bootstrap = new ServerBootstrap();
+        bootstrap.setFactory(new NioServerSocketChannelFactory());
+        bootstrap.setPipelineFactory(new ChannelPipelineFactory() {
+            public ChannelPipeline getPipeline() throws Exception {
+                ChannelPipeline pipeline = Channels.pipeline();
+                pipeline.addLast("decode", new StringDecoder());
+                pipeline.addLast("encode", new StringEncoder());
+                pipeline.addLast("handle", new XndServerHandler());
+                return null;
+            }
+        });
+
+        bootstrap.bind(new InetSocketAddress(8080));
+    }
+}
